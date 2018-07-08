e7960379c4d4a44d23bda65bc08eddff53649b0f
Jianyang Zhang <jianyang212@gmail.com>
2018-07-08 11:02:30 +0800

    push succeed
    push succeed

--- src/tests/tryPush.go
+++ src/tests/tryPush.go
@@ -9,40 +9,51 @@ import (
 	"github.com/google/gopacket/layers"
 )
 
-/* 推送报文 */
+const PROTOCOL_IPV4_TCP string = "ip4:tcp"
+
+/* 测试 */
 func TryPush() {
-	if packet, dstIPaddr, err := initPacket(); err != nil {
+	if packet, err := InitPacket("127.0.0.1", 80, "127.0.0.1", 80); err != nil {
 		panic(err)
 	} else {
-		// 推送
-		fmt.Println("\n报文创建成功, 开始推送...\n ")
-		connection, err := net.ListenPacket("ip4:tcp", "0.0.0.1")
-		if err != nil {
-			panic(err)
-		}
-		_, err = connection.WriteTo(packet.Bytes(), &dstIPaddr)
-		if err != nil {
-			panic(err)
-		}
-		log.Print("发送成功!\n")
+		PushPacket(packet, "127.0.0.1")
 	}
 }
 
-/* 创建报文 */
-func initPacket() (gopacket.SerializeBuffer, net.IPAddr, error) {
-	srcIP := net.ParseIP("127.0.0.1")
-	dstIP := net.ParseIP("127.0.0.1")
+/*
+	推送报文
+*/
+func PushPacket(packet gopacket.SerializeBuffer, ip_dst string) {
+	// 推送
 	dstIPaddr := net.IPAddr{
-		IP: dstIP,
+		IP: net.ParseIP(ip_dst),
 	}
+	fmt.Println("\n报文创建成功, 开始推送...\n ")
+	connection, err := net.ListenPacket(PROTOCOL_IPV4_TCP, "127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+	_, err = connection.WriteTo(packet.Bytes(), &dstIPaddr) // 为什么要传入指针
+	if err != nil {
+		panic(err)
+	}
+	log.Print("发送成功!\n")
+}
+
+/*
+	创建报文
+*/
+func InitPacket(ip_src string, port_src int, ip_dst string, port_dst int) (gopacket.SerializeBuffer, error) {
+	srcIP := net.ParseIP(ip_src)
+	dstIP := net.ParseIP(ip_dst)
 	ipLayer := layers.IPv4{
 		SrcIP:    srcIP,
 		DstIP:    dstIP,
 		Protocol: layers.IPProtocolTCP,
 	}
 	tcpLayer := layers.TCP{
-		SrcPort: layers.TCPPort(80),
-		DstPort: layers.TCPPort(80),
+		SrcPort: layers.TCPPort(port_src),
+		DstPort: layers.TCPPort(port_dst),
 		SYN:     true,
 	}
 	tcpLayer.SetNetworkLayerForChecksum(&ipLayer)
@@ -52,5 +63,5 @@ func initPacket() (gopacket.SerializeBuffer, net.IPAddr, error) {
 		ComputeChecksums: true,
 	}
 	err := gopacket.SerializeLayers(packet, opts, &ipLayer, &tcpLayer)
-	return packet, dstIPaddr, err
+	return packet, err
 }
